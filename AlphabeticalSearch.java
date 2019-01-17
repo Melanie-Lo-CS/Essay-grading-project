d5ea9aa3e6afd68f1cdb32ee3cef62d3b75b69a8
Melanie-Lo-CS <melaniekwanyeelo@gmail.com>
2019-01-17 10:36:56 -0700

    added new files

---
+++ AlphabeticalSearch.java
@@ -0,0 +1,23 @@
+public class AlphabeticalSearch {
+
+  public static void main (String[]args){
+
+boolean String[] word = false;
+boolean Stop = false;
+
+
+    //String[] word = new string[9]
+    String[] word = {"The", "quick", "brown","fox", "jumps", "over", "the", "lazy", "dog."};
+    for (int i=0; i<word.length; i++){
+      System.out.println(word[i]);
+    }
+    for(int i=0;){
+    for array.length-1;
+    while (stop == false){
+      System.out.println(word[0] + "\t" + (word[1]));
+      word[0] = word[0].toLowerCase();
+      System.out.println(word[1].compareTo(word[0]));
+     }
+    }
+  }
+}
