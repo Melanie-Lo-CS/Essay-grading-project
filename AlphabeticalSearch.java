9d344bccf5533a3445aeddca0b8ab1932492852e
Melanie-Lo-CS <melaniekwanyeelo@gmail.com>
2019-02-08 13:34:05 -0700

    ;lpojae tr9euy hwaos;

--- AlphabeticalSearch.java
+++ AlphabeticalSearch.java
@@ -11,19 +11,21 @@ public class AlphabeticalSearch {
 
 
     //String[] word = new string[9]
-    String[] word = {"The", "quick", "brown","fox", "jumps", "over", "the?", "lazy", "dog."};
+    String[] word = {"The", "quick", "brown","fox", "jumps", "over", "the", "lazy", "dog."};
    printArray(word);
     for (int i = 0; i < word.length; i++ ) {
       word[i] = word[i].toLowerCase();
     }
-    System.out.println("\n In lower case");
+    System.out.println("\nIn lower case");
      printArray(word);
      for (int i = 0; i < word.length; i++){
 
        // for periods and symbols
        if(word[i].endsWith(".")) {
          word[i] = word[i].substring(0,word[i].length()-1);
-         System.out.println("\n removed period");
+         System.out.println("\nRemoved period");
+         printArray(word);
+
 }
 }
 
@@ -32,32 +34,26 @@ for (int i = 0; i < word.length; i++){
   // for periods and symbols
   if(word[i].endsWith("?")) {
     word[i] = word[i].substring(0,word[i].length()-1);
-    System.out.println("\n removed ?");
+    printArray(word);
 }
 }
-printArray(word);
+
 for (int i = 0; i < word.length; i++){
 
   // for periods and symbols
   if(word[i].endsWith("!")) {
     word[i] = word[i].substring(0,word[i].length()-1);
-    System.out.println("\n removed !");
+    printArray(word);
 }
 
 
-for (int i = 0; i <word.length; i++) {
-  String temp = "";
-  if(word[i].compareTo(word[i+1]) > word[i+1].compareTo(word[i])) {
-    temp = word[i];
-    word[i] = word[i+1];
-    word[i+1] = temp;
-  }
-  printArray(word);
-}
-}
 }
 
 
 
 }
+
+
+
+
 }
