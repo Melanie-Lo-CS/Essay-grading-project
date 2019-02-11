6612cb0846e54dc511d9916ca93ef8e3cc97502c
Melanie-Lo-CS <melaniekwanyeelo@gmail.com>
2019-02-11 13:17:08 -0700

    dldmclmc

--- AlphabeticalSearch.java
+++ AlphabeticalSearch.java
@@ -8,7 +8,11 @@ public class AlphabeticalSearch {
 
   }
   public static void main (String[]args){
-
+String save;
+Boolean stop = false;
+int compare;
+int compareNumber = 0;
+int stopCount = 0;
 
     //String[] word = new string[9]
     String[] word = {"The", "quick", "brown","fox", "jumps", "over", "the", "lazy", "dog."};
@@ -45,15 +49,30 @@ for (int i = 0; i < word.length; i++){
     word[i] = word[i].substring(0,word[i].length()-1);
     printArray(word);
 }
+}
+while (stop == false) {
+for(int i = 0; i < word.length-1; i++){
+  compare = word[i+1].compareTo(word[i]);
+  compareNumber += 1;
+  if (word[i+1].equals(word[i])) {} else {
+    if ( compare < 0 ) {
+      stopCount += 1;
+      save = word[i+1];
+      word[i+1] = word[i];
+      word[i] = save;
+    }
+    if (stopCount == 0) {
+      stop = true;
+    } else {
+      stopCount = 0;
+    }
+}
+for (String element : word) {
+}
+}
 
 
 }
 
-
-
 }
-
-
-
-
 }
